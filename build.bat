7e0a05a8636a4771fd7cbe698a74811a57d99758
XcomReborn <zachary.coldrick@gmail.com>
2020-01-28 17:10:11 +0000

    updated the build.bat file to auto process rar file

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 pyinstaller --onefile --windowed --icon=coh.ico COHOpponentBot.py
+IF EXIST dist\COHOpponentBot.rar DEL /F dist\COHOpponentBot.rar
+rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt
 pause
