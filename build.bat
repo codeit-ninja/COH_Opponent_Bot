8530cf3f991ae830b166a35bda5cf8c351161cd8
XcomReborn <zachary.coldrick@gmail.com>
2020-02-06 02:18:08 +0000

    removed exe to keep rar files together

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 pyinstaller --onefile --windowed --icon=coh.ico COHOpponentBot.py
 IF EXIST dist\COHOpponentBot.rar DEL /F dist\COHOpponentBot.rar
-rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt dist\overlay.html dist\overlaystyle.css
+rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt overlay.html overlaystyle.css
+del dist\COHOpponentBot.exe
 pause
