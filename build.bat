722fc2c1892d14aaf16f3f8b5850c987303443a2
XcomReborn <zachary.coldrick@gmail.com>
2020-11-02 07:28:08 +0000

    Created single file exe packaged into COHOpponentBot.rar
    Thanks to an updated for the mem_edit python library (version 0.5) the problem packaging using pyinstaller has been overcome.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-pyinstaller --onefile --debug=all --icon=coh.ico COHOpponentBot.py
+pyinstaller --onefile --windowed --icon=coh.ico COHOpponentBot.py
 IF EXIST dist\COHOpponentBot.rar DEL /F dist\COHOpponentBot.rar
 rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt overlay.html overlaystyle.css
 rar a -r dist\COHOpponentBot OverlayImages\*.png 
-REM del dist\COHOpponentBot.exe
+del dist\COHOpponentBot.exe
 pause
