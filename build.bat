6d5066fe2762bc70ef39b24661c73dc633ef743e
XcomReborn <zachary.coldrick@gmail.com>
2020-11-02 10:41:26 +0000

    tempory build with debug enabled
    debug enabled to allow Tleaf to look for errors

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-pyinstaller --onefile --windowed --icon=coh.ico COHOpponentBot.py
+pyinstaller --onefile --debug=all --icon=coh.ico COHOpponentBot.py
 IF EXIST dist\COHOpponentBot.rar DEL /F dist\COHOpponentBot.rar
 rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt overlay.html overlaystyle.css
 rar a -r dist\COHOpponentBot OverlayImages\*.png 
