66d734bee24ccfd88cf79c45050fbda7cd46efee
XcomReborn <zachary.coldrick@gmail.com>
2022-04-13 04:21:41 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-pyinstaller --onefile --windowed --icon=coh.ico COHOpponentBot.py
+pyinstaller --onefile --windowed --icon=Icons/coh.ico COHOpponentBot.py
 IF EXIST dist\COHOpponentBot.rar DEL /F dist\COHOpponentBot.rar
-rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt overlay.html overlaystyle.css
+rar a -ep dist\COHOpponentBot dist\COHOpponentBot.exe dist\Instructions.txt overlay.html Style\OverlayStyle.css
 rar a -r dist\COHOpponentBot OverlayImages\*.png 
 del dist\COHOpponentBot.exe
 pause
