bbf67869947e3d31c5ea9159c641d9f6be25acea
XcomReborn <zachary.coldrick@gmail.com>
2022-04-20 02:11:54 +0100

    Update COHOpponentBot_OverlayTemplates.py

--- Classes/COHOpponentBot_OverlayTemplates.py
+++ Classes/COHOpponentBot_OverlayTemplates.py
@@ -1,6 +1,6 @@
 class OverlayTemplates:
 
-	overlayhtml = """
+    overlayhtml = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,188 +24,187 @@ class OverlayTemplates:
 <body>
 <div class="container">
 <div class = "playerTeam">
-{} 
+{}
 </div>
 <div class = "opponentTeam">
-{} 
+{}
 </div>
 </div>
 <div style="clear: both;"></div>
 </body>
-</html> 
+</html>
 """
 
-	overlaycss = """
+    overlaycss = """
 
 
 
 body {
-	position: relative;
-	background-color: transparent;
-	padding: 0pt;
-	
+    position: relative;
+    background-color: transparent;
+    padding: 0pt;
+
   }
-  
+
 html {
-	position: relative;
-	padding: 0pt;
+    position: relative;
+    padding: 0pt;
 }
-  
-	
+
 .container{width:100%;
-	position: relative;
-	top: 0pt;
-	font-size: 14pt; 
+    position: relative;
+    top: 0pt;
+    font-size: 14pt;
 }
 
 .countryflagimg{
-	position: relative;
-	display: inline;
-	height: 16pt;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    height: 16pt;
+    top: 0pt;
 }
 
 .countryflagimg img{
-	position: relative;
-	display: inline;
-	height:16pt;
-	top: -3pt;
+    position: relative;
+    display: inline;
+    height:16pt;
+    top: -3pt;
 }
 
 
 .factionflagimg{
-	position: relative;
-	display: inline;
-	height: 16pt;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    height: 16pt;
+    top: 0pt;
 }
 
 .factionflagimg img{
-	position: relative;
-	height:18pt;
-	top: -3pt;
+    position: relative;
+    height:18pt;
+    top: -3pt;
 }
 
 .rankimg {
-	position: relative;
-	top: 10pt;
-	height: 30pt;
-	display: inline;
+    position: relative;
+    top: 10pt;
+    height: 30pt;
+    display: inline;
 }
 
 .rankimg img{
-	position: relative;
-	height: 24pt;
-	display: inline;
-	top: -8pt;
+    position: relative;
+    height: 24pt;
+    display: inline;
+    top: -8pt;
 }
 
 .textVariables {
-	position: relative;
-	display: inline;
-	top: -6pt;
+    position: relative;
+    display: inline;
+    top: -6pt;
 }
 
 .name {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .faction {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .matchtype {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .country {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .totalwins {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .totallosses {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .totalwlratio {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .wins {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .losses {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .disputes {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .streak {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .drops {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .rank {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .level {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .wlratio {
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .nonVariableText{
-	position: relative;
-	display: inline;
-	top: -6pt;
+    position: relative;
+    display: inline;
+    top: -6pt;
 }
 
 .steamprofile{
-	position: relative;
-	display: inline;
-	top: 0pt;
+    position: relative;
+    display: inline;
+    top: 0pt;
 }
 
 .cohstatslink{
@@ -216,39 +215,39 @@ top: 0pt;
 
 .opponentTeam {
 
-	  position : absolute;
-	  top: 0pt; 
-	  background-color: rgba(0, 0, 0, 0.5);
-	  color: white;
-	  float: left;
-	  margin-left: 58%;
-	  text-align: left;
-	  padding-bottom: 0pt;
-	  }
+      position : absolute;
+      top: 0pt;
+      background-color: rgba(0, 0, 0, 0.5);
+      color: white;
+      float: left;
+      margin-left: 58%;
+      text-align: left;
+      padding-bottom: 0pt;
+      }
 
 .opponentTeam .name{
 
 color: white;
 
 }
-  
+
 .playerTeam {
 
-	  position: relative;
-	  top: 0pt; 
-	  background-color: rgba(0, 0, 0, 0.5);
-	  color: white;
-	  float: right;
-	  margin-right: 58%;
-	  text-align: right;
-	  padding-bottom: 0pt;
+      position: relative;
+      top: 0pt;
+      background-color: rgba(0, 0, 0, 0.5);
+      color: white;
+      float: right;
+      margin-right: 58%;
+      text-align: right;
+      padding-bottom: 0pt;
 
   }
 
   .playerTeam .name{
 
-	color: white;
-	
+    color: white;
+
 }
 
-	"""
+    """
