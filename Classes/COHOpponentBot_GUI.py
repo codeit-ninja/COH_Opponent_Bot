157900f3e0cdde0b51260b3f7106eedccaaea5dd
XcomReborn <zachary.coldrick@gmail.com>
2022-04-20 05:11:31 +0100

    Update COHOpponentBot_GUI.py

--- Classes/COHOpponentBot_GUI.py
+++ Classes/COHOpponentBot_GUI.py
@@ -29,8 +29,8 @@ class GUI:
     def __init__(self):
 
         # Enter Build Variables for marking About Box
-        self.VersionNumber = "3.0"
-        self.BuildDate = "12-Apr-2022"
+        self.VersionNumber = "3.0.1"
+        self.BuildDate = "20-Apr-2022"
 
         self.ircClient = None  # reference to the opponentbot
 
